c65a9fea6e292984f5a1723541c3b7edef46364f
kevinpirola <gianmarco.laggia@gmail.com>
2017-10-01 19:22:04 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name instrumentsgenealogyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the instrumentsgenealogyApp
+ */
+angular.module('instrumentsgenealogyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
